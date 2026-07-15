bb8c8271aabeec1819052101eaf3317ec104034a
almeida.desenvolve@gmail.com <almeida.desenvolve@gmail.com>
2026-07-15 20:12:10 -0300

    chore: adding  Lock rown in buy route

--- src/main/java/com/example/buy_tickets/repositories/TicketRepository.java
+++ src/main/java/com/example/buy_tickets/repositories/TicketRepository.java
@@ -2,9 +2,21 @@ package com.example.buy_tickets.repositories;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
-
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+import jakarta.persistence.LockModeType;
+import org.springframework.data.jpa.repository.Lock;
 import com.example.buy_tickets.models.TicketEntity;
+import java.util.Optional;
 
 @Repository
 public interface TicketRepository extends JpaRepository<TicketEntity, Long> {
+
+    @Lock(LockModeType.PESSIMISTIC_WRITE)
+    @Query("SELECT t FROM TicketEntity t WHERE t.id = :ticketId")
+    Optional<TicketEntity> findByIdToReserve(@Param("ticketId") Long ticketId);
+
+
+
+
 }
--- src/main/java/com/example/buy_tickets/services/TicketService.java
+++ src/main/java/com/example/buy_tickets/services/TicketService.java
@@ -1,11 +1,38 @@
 package com.example.buy_tickets.services;
 
 import org.springframework.stereotype.Service;
+import com.example.buy_tickets.repositories.TicketRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import com.example.buy_tickets.models.TicketEntity;
+import lombok.extern.slf4j.Slf4j;
+import jakarta.transaction.Transactional;
+
+
 
 @Service
+@Slf4j
 public class TicketService {
 
-    public String buy() {
-        return "hello world";
+    @Autowired
+    private TicketRepository ticketRepository;
+    private TicketEntity reservedTicket;
+    
+    @Transactional
+    public String buy(Long ticketId, Long userId) {
+
+        reservedTicket = isTicketAvailable(ticketId);
+
+        if (reservedTicket != null) {
+            return "CALL the SQS";
+
+        }
+        return "Ticket not available";
+    }
+
+    
+    @Transactional
+    protected TicketEntity isTicketAvailable(Long ticketId) {
+        reservedTicket = ticketRepository.findByIdToReserve(ticketId).orElse(null);
+        return reservedTicket;
     }
 }
