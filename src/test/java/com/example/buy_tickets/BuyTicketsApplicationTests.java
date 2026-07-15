167dda3acd984a1bb2ca1c4164dc3eecd4469d97
almeida.desenvolve@gmail.com <almeida.desenvolve@gmail.com>
2026-07-14 22:10:51 -0300

    Initial commit - Spring Boot API setup

---
+++ src/test/java/com/example/buy_tickets/BuyTicketsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.buy_tickets;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BuyTicketsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
