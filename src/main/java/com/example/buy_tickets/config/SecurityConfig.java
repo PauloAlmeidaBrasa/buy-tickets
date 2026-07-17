4e4c4ab68b405d311af108be5fce3b5fc1c4f44e
almeida.desenvolve@gmail.com <almeida.desenvolve@gmail.com>
2026-07-17 19:20:49 -0300

    fix: fixing CORS

--- src/main/java/com/example/buy_tickets/config/SecurityConfig.java
+++ src/main/java/com/example/buy_tickets/config/SecurityConfig.java
@@ -30,6 +30,7 @@ public class SecurityConfig {
         http
                 .csrf(csrf -> csrf.disable())
                 .authorizeHttpRequests(auth -> auth
+                        .requestMatchers(org.springframework.http.HttpMethod.OPTIONS, "/**").permitAll()
                         .requestMatchers("/api/" + apiVersion + "/auth/login").permitAll()
                         .anyRequest().authenticated()
                 )
