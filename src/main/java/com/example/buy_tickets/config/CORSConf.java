94d067331edfc2427c7e9cc62af2ae03e904828b
almeida.desenvolve@gmail.com <almeida.desenvolve@gmail.com>
2026-07-17 16:29:13 -0300

    feat: adding cors confs

---
+++ src/main/java/com/example/buy_tickets/config/CORSConf.java
@@ -0,0 +1,20 @@
+package com.example.buy_tickets.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class CORSConf implements WebMvcConfigurer {
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+                .allowedOriginPatterns("http://localhost:*")
+                .allowedMethods("GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS")
+                .allowedHeaders("*")
+                .allowCredentials(true)
+                .maxAge(3600);
+    }
+}
